11374943ac37f49e2ae515071906d7bff7966dc7
Cory Perkins <link2cory@gmail.com>
2020-08-10 12:38:18 -0700

    add map with directions to Directions Component

--- src/components/Directions.js
+++ src/components/Directions.js
@@ -1,14 +1,39 @@
 import React, { Component } from "react";
 import { StyleSheet, Text, View, Button } from "react-native";
+import MapView from "react-native-maps";
+import MapViewDirections from "react-native-maps-directions";
 
 import BarList from "./bar-list";
 
+import { GOOGLE_API_KEY } from "../../.env.config";
+
 const Directions = ({ navigation, route }) => {
   const { bars } = route.params;
+  const bar = bars[0];
+  const mapRegion = {
+    latitude: 48.7519,
+    longitude: -122.4787,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  };
   return (
     <View style={styles.container}>
-      <Text>Directions Component</Text>
-      <Text>Placeholder for directions to {bars.pop().name}</Text>
+      <MapView style={styles.map} initialRegion={mapRegion}>
+        <MapViewDirections
+          origin={{
+            latitude: 48.7519,
+            longitude: -122.4787,
+          }}
+          destination={{
+            latitude: bar.geometry.location.lat,
+            longitude: bar.geometry.location.lng,
+          }}
+          apikey={GOOGLE_API_KEY}
+          mode={"WALKING"}
+          strokeWidth={10}
+          strokeColor="green"
+        />
+      </MapView>
     </View>
   );
 };
@@ -18,6 +43,9 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
   },
 });
 
